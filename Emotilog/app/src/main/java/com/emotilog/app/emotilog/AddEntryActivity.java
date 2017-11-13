d7d5e32ca4f85591bd8271914ef4df7231ffebb1
Your Name <451863580@qq.com>
2017-11-13 21:38:39 +0800

    able camera in add entry

--- Emotilog/app/src/main/java/com/emotilog/app/emotilog/AddEntryActivity.java
+++ Emotilog/app/src/main/java/com/emotilog/app/emotilog/AddEntryActivity.java
@@ -2,19 +2,24 @@ package com.emotilog.app.emotilog;
 
 
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -38,9 +43,9 @@ import java.util.Date;
 public class AddEntryActivity extends AppCompatActivity {
     ImageView emojiup;
     private static final int NONE = 0;
-    private static final int PHOTO_GRAPH = 1;// 拍照
-    private static final int PHOTO_ZOOM = 2; // 缩放
-    private static final int PHOTO_RESOULT = 3;// 结果
+    private static final int PHOTO_GRAPH = 1;// take picture
+    private static final int PHOTO_ZOOM = 2; // zoom phtoto
+    private static final int PHOTO_RESOULT = 3;// the result
     private static final String IMAGE_UNSPECIFIED = "image/*";
     private String sdPath;
     private String picPath;
@@ -58,9 +63,9 @@ public class AddEntryActivity extends AppCompatActivity {
 
     }
 
-    public void update(View v){
+    /*public void update(View v){
         dbHelper.onUpgrade(dbHelper.getWritableDatabase(),1,2);
-    }
+    }*/
 
     //when user click on emoji
     public void EmojionClick(View v) {
@@ -129,34 +134,44 @@ public class AddEntryActivity extends AppCompatActivity {
     //when user click take a photo
     public void takeaphoto(View v)
     {
-        String state = Environment.getExternalStorageState();
-        if (state.equals(Environment.MEDIA_MOUNTED)) {
-            //set file name
-            SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
-            Date date = new Date(System.currentTimeMillis());
-            String filename = format.format(date);
-            sdPath = Environment.getExternalStorageDirectory().getPath();
-            picPath = sdPath + "/" + filename + ".png";
-
-
-            //create file to save image in DCIM
-            imageUri = Uri.fromFile(new File(picPath));
-
-            //convert file into uri and set intent to take photo
-
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //照相
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri); //指定图片输出地址
-            startActivityForResult(intent,PHOTO_GRAPH);
-        }
-        else{
-            Toast.makeText(getApplicationContext(), "please ensure SD card",
-                    Toast.LENGTH_LONG).show();
+        if (Build.VERSION.SDK_INT >= 23) {
+            int checkCallPhonePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+            if(checkCallPhonePermission != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},222);
+                return;
+            }else{
+                openCamera();
+            }
+        } else {
+            openCamera();
         }
 
     }
 
+    public void openCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);// call camera
+        startActivityForResult(intent, PHOTO_GRAPH);
+    }
+
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 222:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    openCamera();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(this, "Sorry, you disabled the camera.", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode != RESULT_OK||data==null) {
@@ -166,8 +181,14 @@ public class AddEntryActivity extends AppCompatActivity {
 
         switch (requestCode){
             case PHOTO_GRAPH:
-                File picture = new File(picPath);
-                startPhotoZoom(Uri.fromFile(picture));
+                Bundle bundle = data.getExtras();
+                Bitmap photo_cam = bundle.getParcelable("data");
+                ByteArrayOutputStream stream_cam = new ByteArrayOutputStream();
+                photo_cam.compress(Bitmap.CompressFormat.JPEG, 100, stream_cam);
+                os=stream_cam;
+                ((ImageView)findViewById(R.id.new_entry_photo)).setVisibility(View.VISIBLE);
+                ((ImageView)findViewById(R.id.new_entry_photo)).setImageBitmap(photo_cam); //show the picture
+
                 break;
             case PHOTO_ZOOM:
                 startPhotoZoom(data.getData());
@@ -175,12 +196,12 @@ public class AddEntryActivity extends AppCompatActivity {
             case PHOTO_RESOULT:
                 Bundle extras = data.getExtras();
                 if (extras != null) {
-                    Bitmap photo = extras.getParcelable("data");
-                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                    photo.compress(Bitmap.CompressFormat.JPEG, 100, stream);// (0-100)压缩文件
-                    os=stream;
-                    //此处可以把Bitmap保存到sd卡中，具体请看：http://www.cnblogs.com/linjiqin/archive/2011/12/28/2304940.html
-                    ((ImageView)findViewById(R.id.new_entry_photo)).setImageBitmap(photo); //把图片显示在ImageView控件上
+                    Bitmap photo_al = extras.getParcelable("data");
+                    ByteArrayOutputStream stream_al = new ByteArrayOutputStream();
+                    photo_al.compress(Bitmap.CompressFormat.JPEG, 100, stream_al);
+                    os=stream_al;
+                    ((ImageView)findViewById(R.id.new_entry_photo)).setVisibility(View.VISIBLE);
+                    ((ImageView)findViewById(R.id.new_entry_photo)).setImageBitmap(photo_al); //show the picture
                 }
 
         }
@@ -190,11 +211,11 @@ public class AddEntryActivity extends AppCompatActivity {
         intent.setDataAndType(uri, IMAGE_UNSPECIFIED);
         intent.putExtra("crop", "true");
         // aspectX aspectY 是宽高的比例
-        intent.putExtra("aspectX", 3);
-        intent.putExtra("aspectY", 5);
+        intent.putExtra("aspectX", 4);
+        intent.putExtra("aspectY", 3);
         // outputX outputY 是裁剪图片宽高
-        intent.putExtra("outputX", 300);
-        intent.putExtra("outputY", 500);
+        intent.putExtra("outputX", 400);
+        intent.putExtra("outputY", 300);
         intent.putExtra("return-data", true);
         startActivityForResult(intent, PHOTO_RESOULT);
     }
@@ -227,6 +248,9 @@ public class AddEntryActivity extends AppCompatActivity {
         //插入第一条数据
         db.insert("Entry", null, values);*/
         Toast.makeText(this, "save successfully", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
 
     }
 /*
